802659d6436ec00724d1c1be793a2a1581b911ae
ratakor <thomas.cremel@ratakor.com>
2023-01-02 17:51:18 +0100

    alpha patch on dmenu

--- suckless/dmenu/config.h
+++ suckless/dmenu/config.h
@@ -2,8 +2,6 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static const unsigned int alpha = 0xe0;
-static unsigned int border_width = 0;
 static int centered = 0;                    /* -c option; centers dmenu on screen */
 static int min_width = 400;                    /* 750 minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
@@ -11,6 +9,8 @@ static const char *fonts[] = {
 	/*"monospace:size=10"*/
 	"agave Nerd Font Mono:size=12" //20
 };
+static const unsigned int bgalpha = 0xe0;
+static const unsigned int fgalpha = OPAQUE;
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
@@ -18,18 +18,19 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeSel] = { "#f8f8f2", "#6272a4" },
  	[SchemeOut] = { "#000000", "#00ffff" },
 };
-
 static const unsigned int alphas[SchemeLast][2] = {
-	[SchemeNorm] = { OPAQUE, alpha },
-	[SchemeSel] = { OPAQUE, alpha },
-	[SchemeOut] = { OPAQUE, alpha },
+	/*		fgalpha		bgalphga	*/
+	[SchemeNorm] = { fgalpha, bgalpha },
+	[SchemeSel] = { fgalpha, bgalpha },
+	[SchemeOut] = { fgalpha, bgalpha },
 };
 
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 12; // 18
+static unsigned int lines      = 12; //18
 
 /*
  * Characters not considered part of a word while deleting words
  * for example: " /?\"&[]"
  */
 static const char worddelimiters[] = " ";
+
